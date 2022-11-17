a405a8a1216303ae4a1dcd17347985306ad49634
Marc <nerdfunk@zeitenlos.de>
2022-11-17 22:48:47 +0100

    port channel

--- helper/cisco.py
+++ helper/cisco.py
@@ -1,5 +1,6 @@
 from ciscoconfparse import CiscoConfParse, IPv4Obj
 from netaddr import IPAddress
+import re
 import json
 
 class DeviceConfig:
@@ -22,6 +23,7 @@ class DeviceConfig:
 
     def __parse_config(self):
         IPv4_REGEX = r"ip\saddress\s(\S+\s+\S+)"
+        PO_ACTIVE = r" channel-group\s(\d+)\smode\s(\S+)$"
 
         self.__config["interfaces"] = {}
         interface_cmds = self.__deviceConfig.find_objects(r"^interface ")
@@ -30,8 +32,21 @@ class DeviceConfig:
             self.__config["interfaces"][intf_name] = {}
             self.__config["interfaces"][intf_name]["description"] = "not set"
 
+            # get description
             for cmd in interface_cmd.re_search_children(r"^ description "):
                 self.__config["interfaces"][intf_name]["description"] = cmd.text.strip()[len("description "):]
+
+            # check if LACP port-channel
+            for cmd in interface_cmd.re_search_children(r"^ channel-group"):
+                match = re.match(PO_ACTIVE, cmd.text)
+                if match:
+                    self.__config["interfaces"][intf_name]["po"] = {}
+                    self.__config["interfaces"][intf_name].update({
+                        "po": {
+                            "group": match.group(1),
+                            "mode": match.group(2)
+                        }
+                    })
 
             for cmd in interface_cmd.re_search_children(IPv4_REGEX):
                 ipv4_addr = interface_cmd.re_match_iter_typed(IPv4_REGEX, result_type=IPv4Obj)
