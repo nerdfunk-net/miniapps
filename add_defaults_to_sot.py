94eef995074c7c91bf7b9eebc32e64b1006d7bbf
Marc <mp@nerdfunk.net>
2022-11-23 23:00:30 +0100

    diverses

--- add_defaults_to_sot.py
+++ add_defaults_to_sot.py
@@ -13,7 +13,6 @@ default_config_file = "./config.yaml"
 
 
 def add_defaults_to_sot():
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, required=False)
     parser.add_argument('--repo', type=str, required=False)
@@ -29,35 +28,65 @@ def add_defaults_to_sot():
         config_file = default_config_file
     config = read_config(config_file)
 
-    # get default values of prefixes
+    # get default values from sot
     repo = args.repo or config['files']['sites']['repo']
     filename = args.filename or config['files']['sites']['filename']
-    sites_str = get_file(config["sot"]["api_endpoint"],
-                         repo,
-                         filename)
-    if sites_str is None:
-        print ("%s %s does not exists or could not be read" % (repo, filename))
+    defaults_str = get_file(config["sot"]["api_endpoint"],
+                            repo,
+                            filename)
+    if defaults_str is None:
+        print("%s %s does not exists or could not be read" % (repo, filename))
         sys.exit(-1)
 
-    sites = None
+    # add sites to sot
+    sites = []
+    manufacturers = []
+    roles = []
+    devicetypes = []
+    platforms = []
+
+    # convert defaults to dict
     try:
-        sites_yaml = yaml.safe_load(sites_str)
-        if sites_yaml is not None and 'sites' in sites_yaml:
-           sites = sites_yaml['sites']
+        defaults = yaml.safe_load(defaults_str)
     except Exception as exc:
-        print ("got exception: %s" % exc)
+        print("got exception: %s" % exc)
+
+    # the sites contain the following values:
+    # - manufacturer
+
+    if 'prefixe' in defaults:
+        for prefixe in defaults['prefixe']:
+            if 'site' in defaults['prefixe'][prefixe]:
+                sites.append(defaults['prefixe'][prefixe]['site'])
+            if 'manufacturer' in defaults['prefixe'][prefixe]:
+                manufacturers.append(defaults['prefixe'][prefixe]['manufacturer'])
+            if 'role' in defaults['prefixe'][prefixe]:
+                roles.append(defaults['prefixe'][prefixe]['role'])
+            if 'devicetype' in defaults['prefixe'][prefixe]:
+                devicetypes.append(defaults['prefixe'][prefixe]['devicetype'])
+            if 'platform' in defaults['prefixe'][prefixe]:
+                platforms.append(defaults['prefixe'][prefixe]['platform'])
 
     # we use a dict to store our results
     result = {}
     result['logs'] = []
     result['success'] = []
 
-    for site in sites:
-        data = {'name': site['name'],
-                'slug': site['slug'],
-                'status': site['status']
-        }
-        send_request('addsite',
+    # check if we use some sites that are not configured
+    for a in sites:
+        found = False
+        for b in defaults['sites']:
+            if a == b['name']:
+                found = True
+        if not found:
+            print ("site %s is used but not configured" % a)
+
+    for s in defaults['sites']:
+        data = {'name': s['name'],
+                'slug': s['slug'],
+                'status': s['status']
+                }
+        suc = send_request('addsite',
                      config["sot"]["api_endpoint"],
                      data,
                      result,
@@ -66,26 +95,47 @@ def add_defaults_to_sot():
 
         # check if site already exists
         # if so we update only if user set update to True
-        if not result['success'][0] and not args.update:
-            print(json.dumps(result, indent=4))
-            sys.exit(0)
+        if not suc and args.update:
+            update_data = {}
+            update_list = ['asn', 'time_zone', 'description', 'physical_address',
+                           'shipping_address', 'latitude', 'longitude', 'contact_name',
+                           'contact_phone', 'contact_email', 'comments']
+            for update in update_list:
+                if update in s:
+                    update_data[update] = s[update]
 
-        update_data = {}
-        update_list = ['asn','time_zone','description','physical_address',
-                       'shipping_address','latitude','longitude','contact_name',
-                       'contact_phone','contact_email','comments']
-        for update in update_list:
-            if update in site:
-                update_data[update] = site[update]
+            send_request('updatesite',
+                         config["sot"]["api_endpoint"],
+                         {'slug': s['slug'], 'config': update_data},
+                         result,
+                         'site',
+                         '%s updated' % s['slug'])
 
-        send_request('updatesite',
+    # add manufacturers
+    for m in defaults['manufacturers']:
+        data = {'name': m['name'],
+                'slug': m['slug']}
+        suc = send_request('addmanufacturer',
                      config["sot"]["api_endpoint"],
-                     {'slug': site['slug'], 'config': update_data},
+                     data,
                      result,
-                     'site',
-                     '%s updated' % site['slug'])
+                     'manufacturer',
+                     'manufacturer added')
 
-        print (json.dumps(result,indent=4))
+        if not suc and args.update:
+            update_data = {}
+            update_list = ['slug', 'name', 'description']
+            for update in update_list:
+                if update in m:
+                    update_data[update] = m[update]
+            send_request('updatemanufacturer',
+                         config["sot"]["api_endpoint"],
+                         {'slug': m['slug'], 'config': update_data},
+                         result,
+                         'manufacturer',
+                         '%s updated' % m['slug'])
+
+    print(json.dumps(result, indent=4))
 
 
 if __name__ == "__main__":
--- helper/sot.py
+++ helper/sot.py
@@ -9,19 +9,21 @@ def send_request(url, api_endpoint, json_data, result, item="", success=""):
 
     if r.status_code != 200:
         result['logs'].append('got status code %i' % r.status_code)
+        return False
     else:
         # we got a json. parse it and check if we have a success or not
         response = json.loads(r.content)
         if response["success"]:
             result['success'].append(True)
             result['logs'].append("%s %s" % (item, success))
+            return True
         else:
             result['success'].append(False)
             if "reason" in response:
                 result['logs'].append("%s failed; %s" % (item, response["reason"]))
             else:
-                result['logs'].append("interface updated; unknown reason")
-
+                result['logs'].append("%s updated; unknown reason")
+            return False
 
 def get_file(api_endpoint, repo, filename):
     r = requests.get(url="%s/get/%s/%s" % (api_endpoint, repo, filename))
